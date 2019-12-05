c02a60c79dd69ae2594193135816ab3a83f88cae
Israel Kobler <ikobler@ravtech.co.il>
2019-12-05 10:14:09 +0200

    fix task auto assignee

--- modules/flowable-engine/src/main/java/org/flowable/engine/task/TaskAssigner.java
+++ modules/flowable-engine/src/main/java/org/flowable/engine/task/TaskAssigner.java
@@ -2,6 +2,7 @@ package org.flowable.engine.task;
 
 import org.flowable.engine.delegate.DelegateExecution;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.Map;
 import org.flowable.engine.*;
 import org.flowable.engine.delegate.JavaDelegate;
 import org.flowable.engine.impl.context.Context;
+import org.flowable.engine.impl.util.CommandContextUtil;
 import org.flowable.idm.api.Group;
 import org.flowable.idm.api.User;
 import org.flowable.task.api.Task;
@@ -19,63 +21,75 @@ public class TaskAssigner implements JavaDelegate{
    	
 	    System.out.println("inside TaskAssigner service class:");
 	    
-	    
 	    // get list of all the groups
 	    IdentityService identityService = Context.getProcessEngineConfiguration().getIdentityService();
 	    List<Group> userGroups = identityService.createGroupQuery().list();
-	    
-	    for (int g=0; g<userGroups.size(); g++)
-	    {
-	    	String currentGrouodId = userGroups.get(g).getId();
+		System.out.println("group size "+userGroups.size());
+//		System.out.println("users size "+identityService.createUserQuery().list());
+//		System.out.println("native group size "+ identityService.createNativeGroupQuery().list());
+//		System.out.println("native users group size "+identityService.createNativeUserQuery().list());
+//		Group group = identityService.newGroup("group_"+new Date().getTime());
+//		group.setName("create group "+new Date().getTime());
+//		group.setType("user");
+//		identityService.saveGroup(group);
+//		System.out.println("Create group ");
 
-	    	Map<String, Integer> userTaskCount = new HashMap<String, Integer>();	    
-	    	//  find all group candidate tasks for buyer group
-	    	TaskService taskService = Context.getProcessEngineConfiguration().getTaskService();
+		for (int g=0; g<userGroups.size(); g++) {
+			String currentGrouodId = userGroups.get(g).getId();
 
-	    
-	    	List<Task> groupTasks = taskService.createTaskQuery().taskCandidateGroup(currentGrouodId).list();
-	    	System.out.println(currentGrouodId + " Group have " + groupTasks.size() + " tasks:");	    	    		    	    	
-	    	for (int i=0; i<groupTasks.size(); i++) {
-	    		System.out.println((i+1) + ") " + groupTasks.get(i).getName());	    	    	      
-	    	}
-	    
-	    	// find all users for buyer group
-	    	int totalAssignedTasks = 0;
-	    	identityService = Context.getProcessEngineConfiguration().getIdentityService();
-	    	List<User> users = identityService.createUserQuery().memberOfGroup(currentGrouodId).list();
-	    	System.out.println(currentGrouodId + " Group have " + users.size() + " users:");
-	    	for (int i=0; i<users.size(); i++) {	
-	    		String userId = users.get(i).getId();
-	    		System.out.println((i+1) + ") " + userId);
-	    		int numOfUserTasks = (int) taskService.createTaskQuery().taskAssignee(userId).count();
-	   	      	userTaskCount.put(userId, numOfUserTasks);
-	   	      	totalAssignedTasks = totalAssignedTasks + numOfUserTasks;
-	    	}
+			Map<String, Integer> userTaskCount = new HashMap<String, Integer>();
+			//  find all group candidate tasks for buyer group
+			TaskService taskService = Context.getProcessEngineConfiguration().getTaskService();
 
-	    	int averageNumOfTasks = 0;
-	    	if (users.size() > 0)
-	    		averageNumOfTasks = totalAssignedTasks / users.size();
-	    	for (int i=0; i<groupTasks.size(); i++)
-	    	{
-	    		int j = 0;
-	    		boolean foundUser = false;
-	    	
-	    		while (j<users.size() && !foundUser)
-	    		{
 
-	    			if (userTaskCount.get(users.get(j).getId()) >= averageNumOfTasks)
-	    			{
-	    				groupTasks.get(i).setAssignee(users.get(j).getId());
-	    				totalAssignedTasks++;
-	    				averageNumOfTasks = totalAssignedTasks / users.size();
-	    				foundUser = true;
-	    			}
-	    			else
-	    				j++;
-	    		}
-	    	}
-	    
-	    }
+			List<Task> groupTasks = taskService.createTaskQuery().taskCandidateGroup(currentGrouodId).list();
+			System.out.println(currentGrouodId + " Group have " + groupTasks.size() + " tasks:");
+
+			if (groupTasks.size() > 0) {
+
+				for (int i = 0; i < groupTasks.size(); i++) {
+					System.out.println((i + 1) + ") " + groupTasks.get(i).getName());
+				}
+
+				// find all users for buyer group
+				int totalAssignedTasks = 0;
+				identityService = Context.getProcessEngineConfiguration().getIdentityService();
+				List<User> users = identityService.createUserQuery().memberOfGroup(currentGrouodId).list();
+				System.out.println(currentGrouodId + " Group have " + users.size() + " users:");
+
+				if (users.size() > 0) {
+
+					for (int i = 0; i < users.size(); i++) {
+						String userId = users.get(i).getId();
+						System.out.println((i + 1) + ") " + userId);
+						int numOfUserTasks = (int) taskService.createTaskQuery().taskAssignee(userId).count();
+						userTaskCount.put(userId, numOfUserTasks);
+						totalAssignedTasks = totalAssignedTasks + numOfUserTasks;
+					}
+
+					System.out.println("Total task assigned " + totalAssignedTasks);
+					System.out.println("Total task unassigned " + groupTasks.size());
+
+
+					int averageNumOfTasks = 0;
+					if (users.size() > 0)
+						averageNumOfTasks = (totalAssignedTasks + groupTasks.size()) / users.size();
+					int taskIndex = 0;
+					for (User user : users) {
+						if (userTaskCount.get(user.getId()) < averageNumOfTasks) {
+							int numOfTasksCanAssignee = averageNumOfTasks - userTaskCount.get(user.getId());
+
+							while (taskIndex < groupTasks.size() && numOfTasksCanAssignee > 0) {
+								groupTasks.get(taskIndex).setAssignee(user.getId());
+								taskIndex++;
+								numOfTasksCanAssignee--;
+							}
+						}
+					}
+				}
+
+			}
+		}
 	    	    	    	
     }
 }
